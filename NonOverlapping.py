84bd3802578ecb13cb12ae8ca4e259c6f92ce103
Tanvi <panditanvi@gmail.com>
2018-02-01 01:31:56 +0530

    working for non overlapping with files

--- NonOverlapping.py
+++ NonOverlapping.py
@@ -1,21 +1,53 @@
-from bcl import bcl
+import csv 
+from nltk import *
+import os
 import nltk
+import csv,re
+dataset='dataset.csv'
+output_file='dataset_output.csv'
+verblist="non_overlapping_verbs.csv"
+
+def WriteToFile(output_file,row,verb):
+	with open(output_file,"w") as OP:
+		writer=csv.writer(OP)
+		writer.writerow([row[0],row[1],row[2],verb])
+
+def SelectVerbCategory(verblist,verb):
+	category=[]
+	verb=verb.lower()
+	with open(verblist) as File:
+		reader=csv.reader(File)
+		for row in reader:
+			for word in row:
+				if word==verb:
+					category.append(row[0])
+	File.close()
+	if len(set(category)) ==0:
+		return("Not in list")
+	elif len(set(category)) ==1:
+		return(category[0])
+	else:
+		return("Overlapping")
 
 
-def find_category(verb):
-    count = 0
-    for b in bcl.bloom:
-        if verb in b:
-            return bcl.level[str(count)]
-        count = count + 1
+def processContent(dataset):
+	try:
+		with open(dataset,'r+') as dataset:
+			csvReader=csv.reader(dataset)
+			for row in dataset:
+				tokenData=sent_tokenize(row)   #sentence tokennizer used to split in case of multiplequestions in same sub-questions
+				if len(tokenData) ==1:
+					word_token=word_tokenize(row)
+					pos=pos_tag(word_token)
+					for (word,tag) in pos:
+						if re.match(r"VB[A-Z]*",tag):
+							category=SelectVerbCategory(verblist,word)
+				WriteToFile(output_file,row,category)
+							
+		dataset.close()
+	except Exception as e:
+		print(str(e))
 
-input_question = raw_input("Please Enter A Question : ")
+processContent(dataset)
 
-tokens = nltk.word_tokenize(input_question)
-tagged = nltk.pos_tag(tokens)
 
-for t in tagged:
-    if t[1] == 'VB':
-        category = find_category(t[0])
-        print("\nExtracted Verb : " + str(t[0]))
-        print("\nBloom's Classification Level : " + str(category))
